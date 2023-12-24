b8c54fe8a9d1c710a044c7aa3d945946a2781ee4
davidlgabriel <david.g.leal00@gmail.com>
2023-12-24 14:02:26 +0000

    pontos

--- app/Livewire/Pontos.php
+++ app/Livewire/Pontos.php
@@ -2,10 +2,97 @@
 
 namespace App\Livewire;
 
+use App\Models\Log_Token;
+use App\Models\Tokens;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class Pontos extends Component
 {
+    public $token;
+    public function save()
+    {
+        $log_pontos = Log_Token::where('id_user', Auth::user()->id)
+            ->where('token', $this->token)
+            ->first();
+        $empresa_token = Tokens::where('token', $this->token)->first();
+        $insert_ponto = new Log_Token();
+
+        if (empty($empresa_token)) {
+            echo "<script>
+													alert('Token inválido!');
+												</script>";
+        } else {
+            if (strcmp($empresa_token->descricao, 'jogo') !== 0) {
+                if (strcmp($empresa_token->token, $this->token) == 0) {
+                    # code..
+                    if (!empty($log_pontos->token)) {
+                        if (strcmp($log_pontos->token, $this->token) == 0) {
+                            echo "<script>
+																													alert('Token já introduzido!');
+																												</script>";
+                        } else {
+                            $insert_ponto->id_user = Auth::user()->id;
+                            $insert_ponto->token = $this->token;
+                            $insert_ponto->tipo = $empresa_token->descricao;
+                            $insert_ponto->pontos = $empresa_token->pontos;
+                            $insert_ponto->save();
+                            //Codigo invalido
+                            $user = Auth::user();
+                            $pontosint = $user->pontos;
+                            $total = $pontosint + $empresa_token->pontos;
+                            $user->pontos = $total;
+                            $user->save();
+                            echo "<script>
+																													alert('Token inserido com sucesso!');
+																												</script>";
+                        }
+                    } else {
+                        $insert_ponto->id_user = Auth::user()->id;
+                        $insert_ponto->token = $this->token;
+                        $insert_ponto->tipo = $empresa_token->descricao;
+                        $insert_ponto->pontos = $empresa_token->pontos;
+                        $insert_ponto->save();
+                        //Codigo invalido
+                        $user = Auth::user();
+                        $pontosint = Auth::user()->pontos;
+                        $total = $pontosint + $empresa_token->pontos;
+                        $user->pontos = $total;
+                        $user->save();
+                        if ($user->pontos == 0) {
+                            echo "<script>
+																													alert('O Token já não está válido!');
+																												</script>";
+                        }
+
+                        echo "<script>
+																									alert('Token inserido com sucesso!');
+																								</script>";
+                    }
+                } else {
+                    echo "<script>
+																					alert('Token que introduziu é inválido ou já passou a época festiva!');
+																				</script>";
+                }
+            } else {
+                $id_empres = $empresa_token->id_empresa;
+                ///$questoes = Questao::where('id_empresa', $id_empres)->get();
+                if (empty($log_pontos->token)) {
+                    echo "<script>
+																					alert('Faz a leitura do qr na aplicação do FISTA!');
+																				</script>";
+                    //return redirect()->route('jogo',[ 'q1' => $questoes[0]->id, 'q2' => $questoes[1]->id,'token' => $request->token]);
+                } else {
+                    echo "<script>
+																					alert('Token já introduzido!Tenta outra empresa!');
+																				</script>";
+                }
+            }
+        }
+    }
+
     public function render()
     {
         return view('livewire.pontos');
