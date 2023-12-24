55c9bc4286cff622835c4a08648565d736e2f35d
davidlgabriel <david.g.leal00@gmail.com>
2023-12-24 15:22:08 +0000

    d

--- resources/views/livewire/pontos.blade.php
+++ resources/views/livewire/pontos.blade.php
@@ -1,15 +1,18 @@
-<div>
+<div style="max-width: 500px; margin: auto; padding: 20px;">
     @if (session()->has('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
 
-@if (session()->has('error'))
-    <div class="alert alert-danger">
-        {{ session('error') }}
+    @if (session()->has('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
+    @endif
+
+    <div style="display: flex; justify-content: space-between; align-items: center;">
+        <input type="text" wire:model="token" class="form-control" style="width: 70%; margin-right: 10px;">
+        <button wire:click="save" class="btn btn-primary" style="width: 25%;">Guardar</button>
     </div>
-@endif
-    <input type="text" wire:model="token" class="form-control">
-    <button wire:click="save" class="btn btn-primary">Guardar</button>
 </div>
