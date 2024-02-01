a8f432ed40b78fe3c4aec2fb3181f63d2634b28e
davidlgabriel <david.g.leal00@gmail.com>
2024-02-01 23:54:02 +0000

    ins

--- resources/views/livewire/workshop-inscricao.blade.php
+++ resources/views/livewire/workshop-inscricao.blade.php
@@ -1,7 +1,7 @@
 <div style="display: flex; justify-content: center; align-items: center; margin-top: 1.5rem; margin-bottom: 1.5rem;">
     @if ($workshop->spotsavailable > 0)
         <button wire:click="inscrever" class="btn btn-primary">
-            {{ $estaInscrito ? 'Desinscrever' : 'Inscrever-se no Workshop' }}
+            {{ $estaInscrito ? 'Desinscrever' : 'Inscrever no Workshop' }}
         </button>
     @else
         <button class="btn btn-secondary" disabled>Esgotado</button>
