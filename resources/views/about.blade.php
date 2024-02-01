05f42b356cacd3708ecd22a13301a852605585bf
davidlgabriel <david.g.leal00@gmail.com>
2024-02-01 01:12:23 +0000

    e

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -337,6 +337,103 @@
                     </div>
                 </div>
 
+                <div id="equipa" class="section techwix-team-section techwix-team-section-03 section-padding">
+                    <div class="container">
+                        <div class="team-wrap">
+                            <div class="row text-left" id="" style="margin-top:5rem;">
+                                <h1 class="titl" style="color:black;">Equipa</h1>
+                            </div>
+
+                            <div class="row">
+                                @foreach ($teamData as $data)
+                                    @if ($data['prioridade'] == 1)
+                                        <div class="col-lg-3 col-sm-6">
+                                            <div class="single-team ">
+                                                <div class="team-img">
+                                                    <a href="#">
+                                                        <img src="{{ $data['photo'] }}" alt="" class="team-photo">
+                                                    </a>
+                                                </div>
+
+                                                <div class="team-content">
+                                                    <h3 class="name"><a href="#"> {{ $data['user_name'] }}</a>
+                                                    </h3>
+                                                    <span class="designation">{{ $data['team_name'] }}</span>
+
+                                                    <div class="team-social">
+                                                        <ul class="social">
+                                                            @if (isset($data['linkedin']))
+                                                                <li><a href="{{ $data['linkedin'] }}"><i
+                                                                            class="fab fa-linkedin"></i></a></li>
+                                                            @endif
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endif
+                                @endforeach
+                                @foreach ($teamData as $data)
+                                    @if ($data['prioridade'] == 0)
+                                        <div class="col-lg-3 col-sm-6 team-toggle">
+                                            <div class="single-team ">
+                                                <div class="team-img">
+                                                    <a href="#">
+                                                        <img src="{{ $data['photo'] }}" alt="" class="team-photo">
+                                                    </a>
+                                                </div>
+
+                                                <div class="team-content">
+                                                    <h3 class="name"><a href="#"> {{ $data['user_name'] }}</a>
+                                                    </h3>
+                                                    <span class="designation">{{ $data['team_name'] }}</span>
+                                                    <div class="team-social">
+                                                        <ul class="social">
+                                                            <li><a href="{{ $data['linkedin'] }}"><i
+                                                                        class="fab fa-linkedin"></i></a></li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endif
+                                @endforeach
+
+                                <style>
+                                    .team-toggle {
+                                        display: none;
+                                    }
+
+                                    .team-photo {
+                                        width: 100%;
+                                        height: 323px;
+                                        object-fit: cover;
+                                        object-position: center;
+                                    }
+
+                                    @media (max-width: 767px) {
+                                        .team-photo {
+                                            height: auto;
+                                            /* Ajusta a altura em dispositivos menores para manter a responsividade */
+                                        }
+                                    }
+                                </style>
+
+                            </div>
+
+                            <!-- team-toggle --->
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="more-team-content text-center">
+                                        <p><a href="#" id="more-content-team">Ver Mais <i
+                                                    id="bla"class="fas fa-long-arrow-alt-down"></i></a></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
 
 
@@ -599,35 +696,35 @@
                         </div>
                     </div>
                     <!---<div class="col-lg-4">
-                                                <div class="solution-item solution-item-sm">
-                                                    <div class="solution-img">
-                                                        <a href="#"><img src="assets/images/solution-img2.jpg" alt=""></a>
-                                                    </div>
-                                                    <div class="solution-content">
-                                                        <div class="solution-title">
-                                                            <h3 class="title"><a href="#">AfterMovie</a></h3>
+                                                    <div class="solution-item solution-item-sm">
+                                                        <div class="solution-img">
+                                                            <a href="#"><img src="assets/images/solution-img2.jpg" alt=""></a>
                                                         </div>
-                                                        <a class="play-btn popup-video"
-                                                            href="https://www.youtube.com/watch?time_continue=3&v=_X0eYtY8T_U"><i
-                                                                class="fas fa-play"></i></a>
+                                                        <div class="solution-content">
+                                                            <div class="solution-title">
+                                                                <h3 class="title"><a href="#">AfterMovie</a></h3>
+                                                            </div>
+                                                            <a class="play-btn popup-video"
+                                                                href="https://www.youtube.com/watch?time_continue=3&v=_X0eYtY8T_U"><i
+                                                                    class="fas fa-play"></i></a>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div class="col-lg-4">
-                                                <div class="solution-item solution-item-sm">
-                                                    <div class="solution-img">
-                                                        <a href="#"><img src="assets/images/solution-img2.jpg" alt=""></a>
-                                                    </div>
-                                                    <div class="solution-content">
-                                                        <div class="solution-title">
-                                                            <h3 class="title"><a href="#">Teaser</a></h3>
+                                                <div class="col-lg-4">
+                                                    <div class="solution-item solution-item-sm">
+                                                        <div class="solution-img">
+                                                            <a href="#"><img src="assets/images/solution-img2.jpg" alt=""></a>
                                                         </div>
-                                                        <a class="play-btn popup-video"
-                                                            href="https://www.youtube.com/watch?time_continue=3&v=_X0eYtY8T_U"><i
-                                                                class="fas fa-play"></i></a>
+                                                        <div class="solution-content">
+                                                            <div class="solution-title">
+                                                                <h3 class="title"><a href="#">Teaser</a></h3>
+                                                            </div>
+                                                            <a class="play-btn popup-video"
+                                                                href="https://www.youtube.com/watch?time_continue=3&v=_X0eYtY8T_U"><i
+                                                                    class="fas fa-play"></i></a>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </div>--->
+                                                </div>--->
                 </div>
                 <div class="row" style="margin-top:1rem;">
                     <div class="col-lg-12">
@@ -638,105 +735,6 @@
                     </div>
                 </div>
 
-
-                <div id="equipa" class="section techwix-team-section techwix-team-section-03 section-padding">
-                    <div class="container">
-                        <div class="team-wrap">
-                            <div class="row text-left" id="" style="margin-top:5rem;">
-                                <h1 class="titl" style="color:black;">Equipa</h1>
-                            </div>
-
-                            <div class="row">
-                                @foreach ($teamData as $data)
-                                    @if ($data['prioridade'] == 1)
-                                        <div class="col-lg-3 col-sm-6">
-                                            <div class="single-team ">
-                                                <div class="team-img">
-                                                    <a href="#">
-                                                        <img src="{{ $data['photo'] }}" alt=""
-                                                            class="team-photo">
-                                                    </a>
-                                                </div>
-
-                                                <div class="team-content">
-                                                    <h3 class="name"><a href="#"> {{ $data['user_name'] }}</a>
-                                                    </h3>
-                                                    <span class="designation">{{ $data['team_name'] }}</span>
-
-                                                    <div class="team-social">
-                                                        <ul class="social">
-                                                            @if (isset($data['linkedin']))
-                                                                <li><a href="{{ $data['linkedin'] }}"><i
-                                                                            class="fab fa-linkedin"></i></a></li>
-                                                            @endif
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @endif
-                                @endforeach
-                                @foreach ($teamData as $data)
-                                    @if ($data['prioridade'] == 0)
-                                        <div class="col-lg-3 col-sm-6 team-toggle">
-                                            <div class="single-team ">
-                                                <div class="team-img">
-                                                    <a href="#">
-                                                        <img src="{{ $data['photo'] }}" alt=""
-                                                            class="team-photo">
-                                                    </a>
-                                                </div>
-
-                                                <div class="team-content">
-                                                    <h3 class="name"><a href="#"> {{ $data['user_name'] }}</a>
-                                                    </h3>
-                                                    <span class="designation">{{ $data['team_name'] }}</span>
-                                                    <div class="team-social">
-                                                        <ul class="social">
-                                                            <li><a href="{{ $data['linkedin'] }}"><i
-                                                                        class="fab fa-linkedin"></i></a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    @endif
-                                @endforeach
-
-                                <style>
-                                    .team-toggle {
-                                        display: none;
-                                    }
-
-                                    .team-photo {
-                                        width: 100%;
-                                        height: 323px;
-                                        object-fit: cover;
-                                        object-position: center;
-                                    }
-
-                                    @media (max-width: 767px) {
-                                        .team-photo {
-                                            height: auto;
-                                            /* Ajusta a altura em dispositivos menores para manter a responsividade */
-                                        }
-                                    }
-                                </style>
-
-                            </div>
-
-                            <!-- team-toggle --->
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="more-team-content text-center">
-                                        <p><a href="#" id="more-content-team">Ver Mais <i
-                                                    id="bla"class="fas fa-long-arrow-alt-down"></i></a></p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
 
 
