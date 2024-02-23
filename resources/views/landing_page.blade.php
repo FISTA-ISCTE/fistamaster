2acf289fb62a00c923562ae4de600c8ad00a9f55
davidlgabriel <david.g.leal00@gmail.com>
2024-02-23 17:41:13 +0000

    s

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -47,6 +47,7 @@
 
                         <div class="hero-btn" data-aos="fade-up" data-aos-delay="1000">
                             <a class="btn" href="/become-a-partner">Registar Empresa</a>
+                            <a class="btn" style="margin-left:1.5rem;" href="/registar-user">Registar Aluno</a>
                         </div>
                     </div>
                     <!-- Hero Content End -->
