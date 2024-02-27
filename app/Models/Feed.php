edb3f7fa5d4a76c37943cff251b49ed2aa18d111
davidlgabriel <david.g.leal00@gmail.com>
2024-02-27 22:30:27 +0000

    feed

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Feed extends Model
 {
     use HasFactory;
+
+    public function empresa()
+    {
+        return $this->belongsTo('App\Models\Empresa', 'id_empresa');
+    }
+
 }
