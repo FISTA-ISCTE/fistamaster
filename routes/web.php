53b50cddd56d2979aac573d2d4ae61b3357bef14
davidlgabriel <david.g.leal00@gmail.com>
2024-02-27 17:18:47 +0000

    ap

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,8 @@ use App\Models\Billing;
 use App\Models\Logistica;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Models\ConcursosInscricao;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Barryvdh\DomPDF\Facade as PDF;
 
 
 /*
@@ -364,6 +366,17 @@ Route::get('/registarEmpresa/{name?}', function ($name = null) {
     return view('admin.empresas.registar_info');
 })->name('registarEmpresa');
 
+Route::get('/gerar-pdf', function () {
+    $nome = 'Nome Exemplo'; // Substitua por seu dado dinâmico conforme necessário
+    $data = [
+        'nome' => $nome,
+        'qrCode' => QrCode::size(100)->generate('Conteúdo do QR Code')
+    ];
+
+    $pdf = PDF::loadView('pdf.view', $data);
+    return $pdf->download('exemplo.pdf');
+
+});
 
 Route::get('/sobre-nos', function () {
     $teams = Team::with('user')->get();
