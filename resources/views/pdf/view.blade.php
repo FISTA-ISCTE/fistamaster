d966bebffbe0f2c21ded07ca572ae69a6fd6baed
davidlgabriel <david.g.leal00@gmail.com>
2024-02-27 17:53:21 +0000

    e

--- resources/views/pdf/view.blade.php
+++ resources/views/pdf/view.blade.php
@@ -77,7 +77,9 @@
         <div class="masthead-subheading">
             <img src="img/pdf2.jpg" class="img-responsive"
                 style="width:23rem;height:33rem;border-width:1px; border-style:solid; border-color:#2f7fb0;" />
-            <h2 style="color:#e6d753;">TESTE</h2>
+                <img class="imgB1" src="{{$foto}}" style="width:9.5rem;height:10rem !important;">
+                {!! QrCode::size(200)->generate('Conteúdo do QR Code Aqui') !!}
+                <h2 style="color:#e6d753;">TESTE</h2>
             <h3 class="cargo" style="color:#ffffff;"> Teste </h3>
 
             <h3 class="nome" style="color:#ffffff;">teste</h3>
