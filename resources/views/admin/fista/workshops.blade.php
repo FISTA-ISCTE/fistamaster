d23fa44b391792a5f6ad0c33adb4bd26889c899a
davidlgabriel <david.g.leal00@gmail.com>
2024-02-27 11:14:30 +0000

    g

--- resources/views/admin/fista/workshops.blade.php
+++ resources/views/admin/fista/workshops.blade.php
@@ -1,19 +1,38 @@
-@extends('layouts.app2')
+<x-app-layout>
+    <x-slot name="header">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight" style="margin-left:1rem;font-size:1.5rem;">
+            {{ __('👋 Olá, ') }}<span style="font-size:1.8rem;">{{ Auth::user()->name }}</span>
+        </h1>
+    </x-slot>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
 
-@section('content')
-<div class="container">
-    <h2>Workshops</h2>
-    <div class="row">
-        @foreach ($workshops as $workshop)
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $workshop->name }}</h5>
-                        {!! QrCode::size(200)->generate("https://fista.iscte-iul.pt/D1mC7SLPoT6QYF7ruLhftKYpYCMOgS/workshop/" . $workshop->id) !!}
-                    </div>
-                </div>
-            </div>
-        @endforeach
+    <div class="container">
+        <h2>Workshops</h2>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nome do Workshop</th>
+                    <th>QR Code</th>
+                    <th>Download</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($workshops as $workshop)
+                    <tr>
+                        <td>{{ $workshop->tittle }}</td>
+                        <td>
+                            {!! QrCode::size(200)->generate(
+                                'https://fista.iscte-iul.pt/D1mC7SLPoT6QYF7ruLhftKYpYCMOgS/workshop/' . $workshop->id,
+                            ) !!}
+                        </td>
+                        <td>
+                            <a href="{{ url('/download-qr/' . $workshop->id) }}" class="btn btn-primary">Download QR
+                                Code</a>
+
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-</div>
-@endsection
+</x-app-layout>
